d41015e59e137da9046b8d4f662abfb4b550374a
weixin_global <735369350@qq.com>
2020-02-21 22:14:04 +0800

    项目经理第二次提交项目

--- src/main/java/com/qfjy/controller/HelloController.java
+++ src/main/java/com/qfjy/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/info")
     public  String info(){
-        return "hello spring boot info";
+        return "hello spring boot info 项目经理";
     }
     //开发人员写了一句话
 }
